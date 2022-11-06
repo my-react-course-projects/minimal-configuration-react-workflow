85b6d39a675868b294fefe82f26c68ef0f5016ba
eshwagadkar <eshwagadkar007@gmail.com>
2022-11-06 11:38:02 +0530

    Replace the createRoot with render method

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
